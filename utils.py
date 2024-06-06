abe56f1c3f3cd5a97168b13810bad3e3c1b686d3
Stefan Armbruster <stefan@armbruster-it.de>
2024-06-06 11:18:05 +0200

    adopting loader to sozinianer db

--- utils.py
+++ utils.py
@@ -27,28 +27,30 @@ def extract_title_and_question(input_string):
 
 
 def create_vector_index(driver, dimension: int) -> None:
-    index_query = "CALL db.index.vector.createNodeIndex('stackoverflow', 'Question', 'embedding', $dimension, 'cosine')"
+
+    index_query = "CALL db.index.vector.createNodeIndex('abstracts', 'Abstract', 'embedding', $dimension, 'cosine')"
     try:
         driver.query(index_query, {"dimension": dimension})
     except:  # Already exists
         pass
-    index_query = "CALL db.index.vector.createNodeIndex('top_answers', 'Answer', 'embedding', $dimension, 'cosine')"
-    try:
-        driver.query(index_query, {"dimension": dimension})
-    except:  # Already exists
-        pass
+    # index_query = "CALL db.index.vector.createNodeIndex('top_answers', 'Answer', 'embedding', $dimension, 'cosine')"
+    # try:
+    #     driver.query(index_query, {"dimension": dimension})
+    # except:  # Already exists
+    #     pass
 
 
 def create_constraints(driver):
-    driver.query(
-        "CREATE CONSTRAINT question_id IF NOT EXISTS FOR (q:Question) REQUIRE (q.id) IS UNIQUE"
-    )
-    driver.query(
-        "CREATE CONSTRAINT answer_id IF NOT EXISTS FOR (a:Answer) REQUIRE (a.id) IS UNIQUE"
-    )
-    driver.query(
-        "CREATE CONSTRAINT user_id IF NOT EXISTS FOR (u:User) REQUIRE (u.id) IS UNIQUE"
-    )
-    driver.query(
-        "CREATE CONSTRAINT tag_name IF NOT EXISTS FOR (t:Tag) REQUIRE (t.name) IS UNIQUE"
-    )
+    # driver.query(
+    #     "CREATE CONSTRAINT question_id IF NOT EXISTS FOR (q:Question) REQUIRE (q.id) IS UNIQUE"
+    # )
+    # driver.query(
+    #     "CREATE CONSTRAINT answer_id IF NOT EXISTS FOR (a:Answer) REQUIRE (a.id) IS UNIQUE"
+    # )
+    # driver.query(
+    #     "CREATE CONSTRAINT user_id IF NOT EXISTS FOR (u:User) REQUIRE (u.id) IS UNIQUE"
+    # )
+    # driver.query(
+    #     "CREATE CONSTRAINT tag_name IF NOT EXISTS FOR (t:Tag) REQUIRE (t.name) IS UNIQUE"
+    # )
+    pass
