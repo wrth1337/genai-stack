da3944c1aaf5ba4182a8cc94302ead18f326c56e
Stefan Armbruster <stefan@armbruster-it.de>
2024-08-01 17:44:56 +0200

    feature: dynamic seeding based on available dumps

--- seed-databases.sh
+++ seed-databases.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-echo "Starting database seeding..."
-neo4j-admin database load sozinianer --from-path=/backups
-neo4j-admin database load regestaimperii --from-path=/backups
-echo "Database seeding completed."
-echo "show database" | cypher-shell
-env
-echo "create database \`sozinianer\` if not exists; create database \`regestaimperii\` if not exists;" | cypher-shell 
+# this is called from docker-compose.yml to seed the databases
+# relies on NEO4_ADDRESS, NEO4J_USERNAME, NEO4J_PASSWORD environment variables
+
+find /backups -name "*.dump" | while read i; do
+    dbname=$(basename $i .dump)
+    echo "Starting database $dbname seeding..."
+    neo4j-admin database load "$dbname" --from-path=/backups
+    echo "Database seeding $dbname completed. Created databases (if not exist)"
+    echo "create database \`${dbname}\` if not exists" | cypher-shell
+done
