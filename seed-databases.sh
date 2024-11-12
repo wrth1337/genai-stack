9671186575591121feef2f5a3aa8ff5bf85ec501
Stefan Armbruster <stefan@armbruster-it.de>
2024-11-12 15:57:53 +0300

    cleaning up multi-databases setup

--- seed-databases.sh
+++ seed-databases.sh
@@ -3,10 +3,6 @@
 # this is called from docker-compose.yml to seed the databases
 # relies on NEO4_ADDRESS, NEO4J_USERNAME, NEO4J_PASSWORD environment variables
 
-find /backups -name "*.dump" | while read i; do
-    dbname=$(basename $i .dump)
-    echo "Starting database $dbname seeding..."
-    neo4j-admin database load "$dbname" --from-path=/backups
-    echo "Database seeding $dbname completed. Created databases (if not exist)"
-    echo "create database \`${dbname}\` if not exists" | cypher-shell
-done
+if [ ! -d /data/databases/neo4j ]; then
+  neo4j-admin database load neo4j --from-path=/backups --verbose;
+fi
