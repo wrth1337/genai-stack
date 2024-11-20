a4f09952a4cde949941ca8c3e7742af0d497d364
Stefan Armbruster <stefan@armbruster-it.de>
2024-11-20 15:50:02 +0100

    refactoring retrieval query for a newer db dump

--- backups/download.sh
+++ backups/download.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-curl -o neo4j.dump "https://cloud-ext.mni.thm.de/index.php/s/HqHAQScapZMiZeL/download?path=%2F&files=regestaimperii.dump"
+curl -C - -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36' \
+-o neo4j.dump "https://seafile.rlp.net/f/37059eb1928a428ca6ea/?dl=1"
+#"https://cloud-ext.mni.thm.de/index.php/s/HqHAQScapZMiZeL/download?path=%2F&files=regestaimperii.dump"
